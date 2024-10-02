5308472218db75203cfb77dd3a592a4373f3a5ac
Richardson Schawarski Cruz <125823124+professorrichardson@users.noreply.github.com>
2024-10-01 21:43:36 -0300

    fix users

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -4,7 +4,7 @@ async function quantidadeUsuarios() {
   const res = await fetch(url)
   const dados = await res.json()
   const nomeDasRedes = Object.keys(dados)
-  const quantidadeUsuarios = Object.values(dados)
+  const quantidadeDeUsuarios = Object.values(dados)
 
   const data = [
     {
