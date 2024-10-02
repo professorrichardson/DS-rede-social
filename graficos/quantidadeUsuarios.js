08ae3e4381908f6dffc2ed614330a953a8ee756f
Richardson Schawarski Cruz <125823124+professorrichardson@users.noreply.github.com>
2024-10-01 21:41:23 -0300

    Aula 09 end

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,3 +1,4 @@
+import { getCSS } from "./common.js";
 async function quantidadeUsuarios() {
   const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
   const res = await fetch(url)
@@ -5,18 +6,26 @@ async function quantidadeUsuarios() {
   const nomeDasRedes = Object.keys(dados)
   const quantidadeUsuarios = Object.values(dados)
 
-const data = [
-  {
-    x: nomeDasRedes,
-    y: quantidadeUsuarios,
-    type: 'bar'
-  }
-]
+  const data = [
+    {
+      x: 'nomeDasRedes',
+      y: quantidadeDeUsuarios,
+      type: 'bar',
+      marker: {
+        color: getCSS('--primary-color')
+      }
+    }
+  ]
+
+  const layout = {
+    plot_bgcolor: getCSS('--bg-color'),
+    paper_bgcolor: getCSS('--bg-color')
+}
 
 const grafico = document.createElement('div')
 grafico.className = 'grafico'
 document.getElementById('graficos-container').appendChild(grafico)
-Plotly.newPlot(grafico, data)
+Plotly.newPlot(grafico, data, layout)
 
 }
 
